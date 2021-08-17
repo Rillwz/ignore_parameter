075898a4cb110c6901da1987cad01e350cdd51a7
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-18 04:29:02 +0700

    Ignore Parameter

--- bin/ignore_parameter.dart
+++ bin/ignore_parameter.dart
@@ -1,5 +1,7 @@
 import 'package:ignore_parameter/ignore_parameter.dart' as ignore_parameter;
 
+import 'person.dart';
+
 void main(List<String> arguments) {
-  print('Hello world: ${ignore_parameter.calculate()}!');
+  var p = Person('Joko');
 }
---
+++ bin/person.dart
@@ -0,0 +1,5 @@
+class Person {
+  Person(String name) {
+    print(name);
+  }
+}
