eeddbe23b80dd16d5f3f6ab06487c1216990ee44
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-19 01:05:53 +0700

    ignore parameter Compleated

--- bin/ignore_parameter.dart
+++ bin/ignore_parameter.dart
@@ -3,5 +3,12 @@ import 'package:ignore_parameter/ignore_parameter.dart' as ignore_parameter;
 import 'person.dart';
 
 void main(List<String> arguments) {
-  var p = Person('Joko');
+  var p = Person('Yusril', doingHobbby: (_) {
+    print('do programming');
+  });
+  p.takeARest();
 }
+
+// void davidHobby(String name) {
+//   print('$name is programming');
+// }
--- bin/person.dart
+++ bin/person.dart
@@ -1,5 +1,13 @@
 class Person {
-  Person(String name) {
-    print(name);
+  late String name;
+  Function(String name) doingHobbby;
+
+  Person(this.name, {required this.doingHobbby});
+
+  void takeARest() {
+    // ignore: unnecessary_null_comparison
+    if (doingHobbby != null) {
+      doingHobbby(name);
+    }
   }
 }
